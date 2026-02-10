bb8103e52538371f363ba23971f2a15b0e3a3b58
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2026-02-10 11:34:59 +0000

    task 3 complete with all test pass

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -25,5 +25,24 @@
  * @returns {string} The letter grade or "INVALID"
  */
 export function calculateGrade(score, hasExtraCredit) {
-  // Your code here
+  
+  // score validation
+  if (typeof score !== 'number' || score < 0 || score > 100) return "INVALID";
+
+  // check the extracredit and validate
+  if (hasExtraCredit) {
+    score += 5;
+    if (score > 100) score = 100;
+  }
+  
+  let grade;
+  
+  if (score >= 90 && score <= 100) grade = "A";
+  else if (score >= 80 && score <= 89) grade = "B";
+  else if (score >= 70 && score <= 79) grade = "C";
+  else if (score >= 60 && score <= 69) grade = "D";
+  else if (score >= 0 && score <= 59) grade = "F";
+  else grade = "INVALID"
+
+  return grade;
 }
